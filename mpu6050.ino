ed43ea3997dcc57e724eb882cad783e05dbcce4c
rohitner <rohitner1@gmail.com>
2017-08-13 10:25:58 +0530

    removed code block for running bot

--- mpu6050.ino
+++ mpu6050.ino
@@ -1,25 +1,25 @@
 #include<Wire.h>
 #include <SoftwareSerial.h>
 const int MPU_addr=0x68;
-double AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; 
-uint32_t timer; 
-double compAngleX, compAngleY; 
-#define degconvert 57.2957786 
+double AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+uint32_t timer;
+double compAngleX, compAngleY;
+#define degconvert 57.2957786
 
 SoftwareSerial myserial(10, 11); // RX, TX
 
 void setup() {
   Wire.begin();
   #if ARDUINO >= 157
-  Wire.setClock(400000UL); 
+  Wire.setClock(400000UL);
   #else
-    TWBR = ((F_CPU / 400000UL) - 16) / 2; 
+    TWBR = ((F_CPU / 400000UL) - 16) / 2;
   #endif
 
-  
+
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B); 
-  Wire.write(0);     
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(115200);
   myserial.begin(38400);
@@ -27,10 +27,10 @@ void setup() {
 
 
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  
+  Wire.write(0x3B);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
   Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
@@ -53,57 +53,34 @@ void loop() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  Wire.requestFrom(MPU_addr,14,true);
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
   Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  
-  double dt = (double)(micros() - timer) / 1000000; 
-  timer = micros(); 
+
+  double dt = (double)(micros() - timer) / 1000000;
+  timer = micros();
 
   double roll = atan2(AcY, AcZ)*degconvert;
   double pitch = atan2(-AcX, AcZ)*degconvert;
 
- 
+
   double gyroXrate = GyX/131.0;
   double gyroYrate = GyY/131.0;
 
- 
-  compAngleX = 0.99 * (compAngleX + gyroXrate * dt) + 0.01 * roll; 
-  compAngleY = 0.99 * (compAngleY + gyroYrate * dt) + 0.01 * pitch; 
-  
 
-  Serial.print(compAngleX);Serial.print("\t");
-  Serial.print(compAngleY);Serial.print("\t");
-  if(compAngleX>50)
-  {
-    myserial.write('r');
-    Serial.println("r");
-  }
-  else if(compAngleX<-50)
-  {
-    myserial.write('l');
-    Serial.println("l");
-  }
-  else if(compAngleY>25)
-  {
-    myserial.write('f');
-    Serial.println("f");
-  }
-  else if(compAngleY<-40)
-  {
-    myserial.write('b');
-    Serial.println("b");
-  }
-  else
-  {
-    myserial.write('s');
-    Serial.println("s");
-  }
+  compAngleX = 0.99 * (compAngleX + gyroXrate * dt) + 0.01 * roll;
+  compAngleY = 0.99 * (compAngleY + gyroYrate * dt) + 0.01 * pitch; 
+
+
+  Serial.print(compAngleX);
+  Serial.print("\t");
+  Serial.println(compAngleY);
+
   delay(50);
 
 }
